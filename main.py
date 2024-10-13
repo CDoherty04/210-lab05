06efb0a9dd210812b74b362b49f1d5b9be571fb9
Charlie <charlieedoherty@gmail.com>
2024-10-13 14:27:04 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Name: Charlie Doherty
+KUID: 3115329
+EECS 210 Lab Session: Thursday 4-6
+Lab: 05
+Description: Program that takes a number and produces the next num in lexicographic order
+Collaborators: None
+"""
+
+def next_permutation(num):
+    pass
